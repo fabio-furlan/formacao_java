56a7d46df62d59ef451034c44190a376b1657d2c
Fabio Furlan <furlan.fabio@hotmail.com>
2022-10-17 20:34:12 -0300

    atualizacao

---
+++ bytebank_composto/src/bytebank_composto/TestaBanco.java
@@ -0,0 +1,19 @@
+package bytebank_composto;
+
+public class TestaBanco {
+	
+	public static void main(String[] args) {
+		Cliente paulo = new Cliente();
+		paulo.nome = "Paulo Silveira";
+		paulo.cpf = "222.222.222-22";
+		paulo.profissão = "programador";
+		
+		Conta contaDoPaulo = new Conta();
+		contaDoPaulo.deposita(100);
+		
+		// associa o cliente paulo a conta do paulo
+		contaDoPaulo.titular = paulo;
+		System.out.println(contaDoPaulo.titular.nome);
+	}
+
+}
