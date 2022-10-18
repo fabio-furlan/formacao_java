cc39fb2a63ede12a487a7469724d04b8fb9d92cb
Fabio Furlan <furlan.fabio@hotmail.com>
2022-10-17 21:04:06 -0300

    Sintaxe e var

---
+++ sintaxe_variaveis_e_fluxo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author furla
+ *
+ */
+module sintaxe_variaveis_e_fluxo {
+}
---
+++ sintaxe_variaveis_e_fluxo/src/sintaxe_variaveis_e_fluxo/TestaValores.java
@@ -0,0 +1,22 @@
+package sintaxe_variaveis_e_fluxo;
+
+public class TestaValores {
+	public static void main(String[] args) {
+		int primeiro = 5;
+		int segundo = 7;
+		segundo = primeiro;
+		primeiro = 10;
+		
+		
+		// quanto vale o segundo? R= ele vale 5.
+		System.out.println(segundo);
+		
+		String saudacao = "Olá, meu nome é ";
+		String nome = "Rômulo ";
+		String continuacao = "e minha idade é ";
+		int idade = 100;
+		System.out.println(saudacao+nome+continuacao+idade);
+		
+	}
+
+}
